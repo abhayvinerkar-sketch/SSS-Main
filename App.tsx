73e794c090503a4d4103514ef21009b498ba82a1
abhayvinerkar-sketch <abhay.vinerkar@gmail.com>
2026-09-02 22:09:25 +0530

    Hide Android navigation bar on app launch

--- App.tsx
+++ App.tsx
@@ -1,2 +1,2 @@
-import React from'react';import{NavigationContainer}from'@react-navigation/native';import{AppNavigator}from'./src/navigation/AppNavigator';import{ThemeProvider}from'./src/theme/ThemeProvider';
-export default function App(){return <ThemeProvider><NavigationContainer><AppNavigator/></NavigationContainer></ThemeProvider>}
+import React,{useEffect}from'react';import{NavigationContainer}from'@react-navigation/native';import{AppNavigator}from'./src/navigation/AppNavigator';import{ThemeProvider}from'./src/theme/ThemeProvider';import*as NavigationBar from'expo-navigation-bar';
+export default function App(){useEffect(()=>{NavigationBar.setVisibilityAsync('hidden').catch(()=>{});NavigationBar.setBehaviorAsync('overlay-swipe').catch(()=>{});return()=>{NavigationBar.setVisibilityAsync('visible').catch(()=>{})}},[]);return <ThemeProvider><NavigationContainer><AppNavigator/></NavigationContainer></ThemeProvider>}
